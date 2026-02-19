56cdffc8c4c0ac43e73b5a8d53f0a1851075cadf
zarifjonn025-boop <zarifjonn025@gmail.com>
2026-02-19 09:42:53 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
+import telebot
 
+BOT_TOKEN = "BU YERGA TOKEN"
+
+bot = telebot.TeleBot(BOT_TOKEN)
+
+@bot.message_handler(commands=['start'])
+def start(message):
+    bot.send_message(message.chat.id, "Bot ishlayapti ✅")
+
+bot.polling()
